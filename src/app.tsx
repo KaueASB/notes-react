39eaf54f20f4509d056fb8ee142ad729e151b4f0
Kaue Alves <kaue_alves00@yahoo.com>
2024-02-13 00:13:59 -0300

    style: responsive layout

--- src/app.tsx
+++ src/app.tsx
@@ -48,7 +48,7 @@ export function App() {
       : notes
 
   return (
-    <div className="mx-auto my-12 max-w-6xl space-y-6">
+    <div className="mx-auto my-12 max-w-6xl space-y-6 px-5">
       <img src={logo} alt="logo nlw expert" />
 
       <form className="w-full">
@@ -62,7 +62,7 @@ export function App() {
 
       <div className="h-px bg-slate-700" />
 
-      <div className="grid auto-rows-[250px] grid-cols-3 gap-6">
+      <div className="grid auto-rows-[250px] grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
         <NewNoteCard onNoteCreated={onNoteCreated} />
 
         {notes &&
